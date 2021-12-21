78185ceec746e56aeb09ee82bcd83da85b0e5846
Pardeep <pardeep@sweetlegs.ca>
2021-12-21 10:39:35 -0800

    bug fixing

--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -1,49 +1,43 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default{
+export default {
   login: user => {
-    return fetch('/user/login',{
+    console.log(user);
+    return fetch('/user/login', {
       method: "post",
       body: JSON.stringify(user),
-      header: {
+      headers: {
         'Content-Type': 'application/json'
       }
     }).then(res => {
-      if (res.status !== 401) {
+      if (res.status !== 401)
         return res.json().then(data => data);
-      } else {
-        return { isAuthenticated: false, user: { username: "", role: "" } }
-      }
-    });
+      else
+        return { isAuthenticated: false, user: { username: "", role: "" } };
+    })
   },
   register: user => {
+    console.log(user);
     return fetch('/user/register', {
       method: "post",
       body: JSON.stringify(user),
-      header: {
+      headers: {
         'Content-Type': 'application/json'
       }
-    }).then(res => {
-      console.log("res", res);
-      if (res.status !== 401) {
-        return res.json().then(data => data);
-      } else {
-        return { isAuthenticated: false, user: { username: "", role: "" } }
-      }
-    })
+    }).then(res => res.json())
+      .then(data => data);
   },
   logout: () => {
     return fetch('/user/logout')
       .then(res => res.json())
       .then(data => data);
   },
-  isAuthenticated: () =>{
+  isAuthenticated: () => {
     return fetch('/user/authenticated')
-            .then(res => {
-              if(res.status !== 401){
-                return res.json().then(data => data);
-              }else{
-                return {isAuthenticated: false, user: {username: "", role: ""}}
-              }
-            });
+      .then(res => {
+        if (res.status !== 401)
+          return res.json().then(data => data);
+        else
+          return { isAuthenticated: false, user: { username: "", role: "" } };
+      });
   }
+
 }
